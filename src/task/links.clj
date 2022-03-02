d7c32dc90d2dda14e61330a5a9d291eb9d14110d
Ilya Dubov <pikkvile@gmail.com>
2022-03-02 14:45:13 +0300

    readme

--- src/task/links.clj
+++ src/task/links.clj
@@ -1,12 +1,5 @@
 (ns task.links)
 
-; The task is to write a function which will traverse renterswarehouse.com website
-; and collect all "home for rent" links, such as for example
-; https://www.renterswarehouse.com/rental-listings/13953
-; https://www.renterswarehouse.com/rental-listings/16760
-; https://www.renterswarehouse.com/rental-listings/16786
-; etc
-
 (def base "https://renterswarehouse.com")
 
 (defn fetch-all-links)
