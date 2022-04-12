22091865782e53103f006890bc87807ba7acafa3
Nícolas Sims Botelho <nbotelho.dev@gmail.com>
2022-04-12 10:46:25 -0300

    fix: removing dynamic mark

--- src/task/links.clj
+++ src/task/links.clj
@@ -3,14 +3,14 @@
     [task.office :as office]
     [task.rent-place :as rent-place]))
 
-(def ^:dynamic *base-url* "https://renterswarehouse.com")
+(def base-url "https://renterswarehouse.com")
 
 (defn fetch-all-links
   []
-  (->> (office/fetch-all *base-url*)
-       (pmap (partial office/fetch-geolocation *base-url*))
-       (pmap (partial rent-place/fetch-all *base-url*))
-       (rent-place/make-rental-link-list *base-url*)))
+  (->> (office/fetch-all base-url)
+       (pmap (partial office/fetch-geolocation base-url))
+       (pmap (partial rent-place/fetch-all base-url))
+       (rent-place/make-rental-link-list base-url)))
 
 (comment
   (fetch-all-links))
